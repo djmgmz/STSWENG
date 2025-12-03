476a8825486ccf9ed31bd8f2107f7e9584c7739e
Dominic Gomez <dominic_joel_gomez@dlsu.edu.ph>
2025-12-03 08:23:29 +0800

    feat: added image uploads to blogging

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
